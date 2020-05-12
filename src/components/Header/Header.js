23e44c318798f4dd5a63386efe94b9daef6fb5ba
Gaurav Pargaonkar <gauravpargaonkar93@gmail.com>
2020-05-12 22:51:01 +0530

    changes done with select country

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div className="header">
             <h1>COVI-DASH</h1>
+            <Link to="/country">
+                <div>See country specific data</div>
+            </Link>
         </div>
     )
 }
