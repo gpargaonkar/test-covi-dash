cd142ec4b1412dea3498358271968430739eb6a0
Gaurav Pargaonkar <gauravpargaonkar93@gmail.com>
2020-05-12 20:18:46 +0530

    Updates

---
+++ precache-manifest.8a739c3ce7999a827eb8ec8be4fb80a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f550eb99ce87e97b4f9f5b9ddb7cecd",
+    "url": "/test-covi-dash/index.html"
+  },
+  {
+    "revision": "952ec9dc22d8ec006810",
+    "url": "/test-covi-dash/static/css/main.e24a9153.chunk.css"
+  },
+  {
+    "revision": "5534833e6cd19bc9b4c9",
+    "url": "/test-covi-dash/static/js/2.6ed7832f.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/test-covi-dash/static/js/2.6ed7832f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952ec9dc22d8ec006810",
+    "url": "/test-covi-dash/static/js/main.a1991fb0.chunk.js"
+  },
+  {
+    "revision": "3973e56ec1f77222fa94",
+    "url": "/test-covi-dash/static/js/runtime-main.14d8ed82.js"
+  }
+]);
